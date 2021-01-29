2120d8ed82be94558c41d54fab636bfd54badf24
Kemian-JMO <55388741+Kemian-JMO@users.noreply.github.com>
2021-01-29 13:32:17 +0000

    Update development.php

--- Software_Code/development.php
+++ Software_Code/development.php
@@ -1,14 +1,25 @@
-<script type="text/javascript" src="file.js"></script>
-<head>
-
-<div class="form-group">
-<div name="title-question" id="temp"><b>uwu this better work</b>
-    <input type="email" class="form-control" placeholder="Enter email" id="email">
-  </div>
-  <script>
-    var newID = innerID;
-    var div = document.getElementById("temp")
-    div.setAttribute("id", newID);
-    console.log(newID)
-  </script>
-<script>
+<script type="text/javascript" src="file.js"></script>
+
+<head>
+
+
+
+<div class="form-group">
+
+<div name="title-question" id="temp"><b>uwu this better work</b>
+
+    <input type="email" class="form-control" placeholder="Enter email" id="email">
+
+  </div>
+
+  <script>
+
+    var newID = innerID;
+
+    var div = document.getElementById("temp")
+
+    div.setAttribute("id", newID);
+
+    console.log(newID)
+
+  </script>
